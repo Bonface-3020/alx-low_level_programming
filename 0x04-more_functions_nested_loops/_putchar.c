8928c4b10079eb8e219c2f65c7ec57e5c9f327bb
bonface <thuobonface42@gmail.com>
2023-02-23 09:05:43 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,15 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
-int _putchar(char c)
+void print_numbers(void)
 {
-	return (write(1, &c, 1));
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
 }
+_putchar('\n');
+}
+
