3e4e9af3133ce49493e51da3b347e83518b3584b
bonface <thuobonface42@gmail.com>
2023-02-24 01:46:13 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_most_numbers(void)
 {
-int c;
-for (c = '0'; c <= '9'; c++)
+int i;
+for (i = 0; i < 10; i++)
 {
-if (c != '2' && c != '4')
+if (i != 2 && i != 4)
 {
-_putchar(c);
+_putchar(i + '0');
+}
 }
 _putchar('\n');
-}
 }
